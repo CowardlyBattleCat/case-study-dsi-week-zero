a291384baf8c8ebf1a07f85e5f796db48a8458c5
CowardlyBattleCat <thomas.harlan.adams@gmail.com>
2018-09-14 13:01:46 -0700

    Modify import_clean_functions.py. Version has import function, general clean_data functions, and 4 functions to be passed to clean_data.

--- import_clean_functions.py
+++ import_clean_functions.py
@@ -2,26 +2,41 @@
 
 def load_csv_as_dict(file_name_path):
     """Function to take in csv data and return a list of dictionaries with
-    header: value as key: value pairs."""
+    header: value as (key: value) pairs."""
     with open (file_name_path) as f:
         reader = csv.DictReader(f)
         data = list(reader)
     return data
 
 def clean_data(data, cleaning_function):
-    """Function to apply a specific cleaning function to data row by row"""
+    """Function to apply a specific cleaning function to data row by row."""
     return [cleaning_function(row) for row in data]
 
-def clean_square_footage(row):
-    """Function to clean 'SqFtTotLiving' of a single dictionary. Intended to be passed to clean_data function."""
-    out_row = row.copy()
-    out_row['SqFtTotLiving'] = int(out_row['SqFtTotLiving'])
-    return out_row
-
 def combine_major_minor(row):
-    """Function to clean merge major and minor of a single dictionary. Intended to be passed to clean_data function."""
+    """Function to merge 'Major' and 'Minor' into a single (key: value) pair with
+    key = 'major_minor'. Intended to be passed to clean_data function."""
     out_row = row.copy()
     out_row['major_minor'] = tuple((out_row['Major'], out_row['Minor']))
     del out_row['Major']
     del out_row['Minor']
     return out_row
+
+def clean_square_footage(row):
+    """Function to clean 'SqFtTotLiving' of a single dictionary. Intended to be
+    passed to clean_data function."""
+    out_row = row.copy()
+    out_row['SqFtTotLiving'] = int(out_row['SqFtTotLiving'])
+    return out_row
+
+def clean_date(row):
+    """Function to clean 'DocumentDate' of a single dictionary. Intended to be
+    passed to clean_data function."""
+    output = row.copy()
+    output['DocumentDate'] = output['DocumentDate'][-4:]
+    return output
+
+def clean_sales_price(row):
+   """Function to clean 'SalePrice' of a single dictionary. Intended to be passed to clean_data function."""
+   out_row = row.copy()
+   out_row['SalePrice'] = int(out_row['SalePrice'])
+   return out_row
