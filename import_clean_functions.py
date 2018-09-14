c54a814acf5ae0425d18e30b95b098524a68db17
CowardlyBattleCat <thomas.harlan.adams@gmail.com>
2018-09-14 12:40:13 -0700

    Update import_clean_function.py to add combine_major_minor function and fix clean_square_footage function

--- import_clean_functions.py
+++ import_clean_functions.py
@@ -15,6 +15,13 @@ def clean_data(data, cleaning_function):
 def clean_square_footage(row):
     """Function to clean 'SqFtTotLiving' of a single dictionary. Intended to be passed to clean_data function."""
     out_row = row.copy()
-    for key, val in out_row.items():
-        out_row['SqFtTotLiving'] = int(out_row['SqFtTotLiving'])
+    out_row['SqFtTotLiving'] = int(out_row['SqFtTotLiving'])
+    return out_row
+
+def combine_major_minor(row):
+    """Function to clean merge major and minor of a single dictionary. Intended to be passed to clean_data function."""
+    out_row = row.copy()
+    out_row['major_minor'] = tuple(out_row['Major'], out_row['Minor'])
+    del out_row['Major']
+    del out_row['Minor']
     return out_row
